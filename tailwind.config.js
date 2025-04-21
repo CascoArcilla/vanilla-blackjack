fe098336581cf670a45e336497a634abdabf01a3
CascoArcilla <normon1230@gmail.com>
2025-04-20 20:41:19 -0600

    Create class Card

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Ajusta según tus archivos (ej: "./src/**/*.html")
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
