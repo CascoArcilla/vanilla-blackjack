89747b9e9ce24d8b6908ca589d2c73b0f50eaf0c
CascoArcilla <normon1230@gmail.com>
2025-04-21 14:18:22 -0600

    change to Vite

--- js/Suit.js
+++
@@ -1,6 +0,0 @@
-export const Suit = {
-  CLUP:"clup",
-  DAIMOND:"daimond",
-  HEART:"heart",
-  SPADES:"spades",
-};
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html"], // Ajusta según tus archivos (ej: "./src/**/*.html")
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
